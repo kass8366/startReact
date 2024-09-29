5e7da1c25414561af3d0e1e7d991bf1dc4fe3d3f
kass8366 <156824078+kass8366@users.noreply.github.com>
2024-09-29 17:59:57 +0300

    HomeWork23

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,20 @@
 import okCircle from './svg/ok-circle.svg';
-import Styled from './main.module.css'
+import Styled from './main.module.css';
+import data from './data.json';
+
+function cardPaint(data){
+    return (
+        data.map(function(elem) {
+            return (
+                <div className={Styled['card_container']}>
+                    <h2 className={Styled['card_container__title']} >{elem.title}</h2>
+                    <img className={Styled['card_container__img']} src={elem.url} />
+                    <p className={Styled['card_container__info']} >{elem.info}</p>
+                </div>
+            )
+        })
+    )
+}
 
 export function Main () {
     return (
@@ -8,6 +23,7 @@ export function Main () {
                 Я кнопка с галочкой
                 <img src={okCircle} />
             </button> 
+            {cardPaint(data)}
         </div>
     );
   }
